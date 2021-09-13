67f121c3995daab123996c35d8dafc0146ab8e7b
Beet-le <1693332907@qq.com>
2021-09-13 11:18:08 +0800

    913

---
+++ servlet_01/src/main/java/com/bt/servet/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.bt.servet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+//        ServletOutputStream outputStream = resp.getOutputStream();
+        System.out.println("进入DoGet方法");
+        PrintWriter writer = resp.getWriter();//响应流
+        writer.print("Hello , Servlet");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
