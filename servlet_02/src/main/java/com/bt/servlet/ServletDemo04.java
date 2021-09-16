49ace0dce021622dd8c4b6fbc8f0337ff0ce5147
Beet-le <1693332907@qq.com>
2021-09-16 09:38:28 +0800

    914

--- servlet_02/src/main/java/com/bt/servlet/ServletDemo04.java
+++ servlet_02/src/main/java/com/bt/servlet/ServletDemo04.java
@@ -7,14 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ServletDemo03 extends HttpServlet {
+public class ServletDemo04 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext context=this.getServletContext();
         //Parameter 参数
-        context.getInitParameter("url");
-        String url=context.getInitParameter("url");
-        resp.getWriter().print(url);
+        context.getRequestDispatcher("/gp");
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
