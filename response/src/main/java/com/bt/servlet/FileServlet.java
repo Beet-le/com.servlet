1a051df4a4dee5ec18f345050d589278d68ede53
Beet-le <1693332907@qq.com>
2021-09-21 13:15:49 +0800

    914

--- response/src/main/java/com/bt/servlet/FileServlet.java
+++ response/src/main/java/com/bt/servlet/FileServlet.java
@@ -7,17 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 public class FileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //    要获取下载文件的路径
-        String realPath = this.getServletContext().getRealPath("/1.png");
+//        String realPath = this.getServletContext().getRealPath("/1.png");
+        String realPath = "E:\\IDEAProjects\\javaweb\\com.servlet\\response\\src\\main\\resources\\1.png";
         System.out.println("获取下载文件的路径:"+realPath);
 //    下载的文件名是啥？
         String fileName = realPath.substring(realPath.lastIndexOf("\\" + 1));
 //    设置想办法让浏览器能够支持下载(Content-Disposition)我们需要的东西
-        resp.setHeader("Content-Disposition","attachment;filename="+fileName);
+        resp.setHeader("Content-Disposition","attachment;filename="+ URLEncoder.encode(fileName,"utf-8"));
 //     获取下载文件的输入流
         FileInputStream in=new FileInputStream(realPath);
 //     创建缓冲区
