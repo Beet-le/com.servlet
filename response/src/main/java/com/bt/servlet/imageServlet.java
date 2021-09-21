96e77cb58f4432476c1e5a2efcdf623d5b498899
Beet-le <1693332907@qq.com>
2021-09-21 23:15:49 +0800

    914

--- response/src/main/java/com/bt/servlet/imageServlet.java
+++ response/src/main/java/com/bt/servlet/imageServlet.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class imageServlet extends HttpServlet {
@@ -11,6 +13,13 @@ public class imageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //如何让浏览器3秒自动刷新一次;
         resp.setHeader("refresh","3");
+        //在内存中创建一个图片
+        BufferedImage image = new BufferedImage(80,20,BufferedImage.TYPE_INT_RGB);
+        //得到图片
+        Graphics2D g = (Graphics2D) image.getGraphics(); //笔
+        //设置图片的背景颜色
+        g.setColor(Color.BLACK);
+
     }
 
     @Override
