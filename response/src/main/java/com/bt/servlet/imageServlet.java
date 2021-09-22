70efd947f50b483718f60c536f08bee4611fc121
Beet-le <1693332907@qq.com>
2021-09-22 20:48:08 +0800

    922

--- response/src/main/java/com/bt/servlet/imageServlet.java
+++ response/src/main/java/com/bt/servlet/imageServlet.java
@@ -1,5 +1,6 @@
 package com.bt.servlet;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +15,26 @@ public class imageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //如何让浏览器3秒自动刷新一次;
         resp.setHeader("refresh","3");
+
         //在内存中创建一个图片
         BufferedImage image = new BufferedImage(80,20,BufferedImage.TYPE_INT_RGB);
         //得到图片
         Graphics2D g = (Graphics2D) image.getGraphics(); //笔
         //设置图片的背景颜色
-        g.setColor(Color.BLACK);
+        g.setColor(Color.white);
+        g.fillRect(0,0,80,20);
+        //给图片写数据
+        g.setColor(Color.BLUE);
+        g.setFont(new Font(null,Font.BOLD,20));
+        g.drawString(makeNum(),0,20);
+        //告诉浏览器，这个请求用图片的方式打开
+        resp.setContentType("image/jpeg");
+        //网站存在缓存，不让浏览器缓存
+        resp.setDateHeader("expires",-1);
+        resp.setHeader("Cache-Control","no-cache");
+        resp.setHeader("Pragma","no-cache");
+        //把图片写给浏览器
+        ImageIO.write(image,"jpg", resp.getOutputStream());
 
     }
     private String makeNum(){
